24da4921a9b6d1a025ab8826fdd9666cbc2622cc
Nlb0sl <145720901+nibosl@users.noreply.github.com>
2025-01-17 16:30:39 +0900

    Create config.py
    .envファイルから情報を読み取る

---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+steam_key = os.getenv("steam_key")
+steam_id = os.getenv("steam_id")
+
+ntn_key = os.getenv("ntn_key")
+ntn_dbid = os.getenv("ntn_dbid")
